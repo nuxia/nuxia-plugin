1ee30cf6e6da6b5ad24aeead376fc94cdf8a1acb
Yannick Snobbert <yannick.snobbert@gmail.com>
2015-07-08 14:26:55 +0200

    [IMPROVEMENT] Refactoring

--- src/Nuxia/Bundle/NuxiaBundle/DependencyInjection/NuxiaExtension.php
+++ src/Nuxia/Bundle/NuxiaBundle/DependencyInjection/NuxiaExtension.php
@@ -7,11 +7,6 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-/**
- * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
- */
 class NuxiaExtension extends Extension
 {
     /**
@@ -24,8 +19,8 @@ class NuxiaExtension extends Extension
 
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services/default.yml');
-        $this->loadSecurity($config, $container, $loader);
-        $this->loadMedia($config, $container, $loader);
+        $this->registerSecurityConfiguration($config, $container, $loader);
+        $this->registerMediaConfiguration($config, $container, $loader);
         $loader->load('services/doctrine.yml');
         $loader->load('services/form.yml');
         $loader->load('services/twig.yml');
@@ -37,21 +32,31 @@ class NuxiaExtension extends Extension
         if (!isset($config['paginator']['enabled']) || $config['paginator']['enabled'] === true) {
             $loader->load('services/paginator.yml');
         }
-        $this->loadValidator($config, $container, $loader);
-        $this->loadMailer($config, $container, $loader);
+        $this->registerValidatorConfiguration($config, $container, $loader);
+        $this->registerMailerConfiguration($config, $container, $loader);
     }
 
-    private function loadSecurity(array &$config, ContainerBuilder $container, YamlFileLoader $loader)
+    /**
+     * @param  array            $config
+     * @param  ContainerBuilder $container
+     * @param   YamlFileLoader   $loader
+     */
+    private function registerSecurityConfiguration(array &$config, ContainerBuilder $container, YamlFileLoader $loader)
     {
         if ($config['security']['enabled']) {
             $container->setParameter('nuxia.security.disable_password', $config['security']['disable_password']);
             $loader->load('services/security.yml');
-            //$this->loadLogin($config, $container, $loader);
+            //$this->registerLoginConfiguration($config, $container, $loader);
         }
         unset($config['security']);
     }
 
-    private function loadLogin(array &$config, ContainerBuilder $container, YamlFileLoader $loader)
+    /**
+     * @param  array            $config
+     * @param  ContainerBuilder $container
+     * @pram   YamlFileLoader   $loader
+     */
+    private function registerLoginConfiguration(array &$config, ContainerBuilder $container, YamlFileLoader $loader)
     {
         if ($config['security']['login']['enabled']) {
             $loader->load('services/login.yml');
@@ -59,20 +64,35 @@ class NuxiaExtension extends Extension
         unset($config['security']['login']);
     }
 
-    private function loadMedia(array &$config, ContainerBuilder $container, YamlFileLoader $loader)
+    /**
+     * @param  array            $config
+     * @param  ContainerBuilder $container
+     * @pram   YamlFileLoader   $loader
+     */
+    private function registerMediaConfiguration(array &$config, ContainerBuilder $container, YamlFileLoader $loader)
     {
         $container->setParameter('nuxia.media.thumbnail_path', $config['media']['thumbnail_path']);
         unset($config['media']);
     }
 
-    private function loadMailer(array &$config, ContainerBuilder $container, YamlFileLoader $loader)
+    /**
+     * @param  array            $config
+     * @param  ContainerBuilder $container
+     * @pram   YamlFileLoader   $loader
+     */
+    private function registerMailerConfiguration(array &$config, ContainerBuilder $container, YamlFileLoader $loader)
     {
         $from = $config['mailer']['from'];
         $container->setParameter('mailer.from', array($from['email'] => $from['name']));
         unset($config['email']['from']);
     }
 
-    private function loadValidator(array &$config, ContainerBuilder $container, YamlFileLoader $loader)
+    /**
+     * @param  array            $config
+     * @param  ContainerBuilder $container
+     * @pram   YamlFileLoader   $loader
+     */
+    private function registerValidatorConfiguration(array &$config, ContainerBuilder $container, YamlFileLoader $loader)
     {
         $loader->load('services/validator.yml');
         $container->setParameter('validator.reserved_words', $config['validator']['reserved_words']);
