f1dcfd0aa1d3ce536ad23009f5580db5786da94b
Yannick Snobbert <yannick.snobbert@gmail.com>
2015-12-08 15:18:40 +0100

    Php doc

--- src/Nuxia/Component/Form/Factory/FormFactory.php
+++ src/Nuxia/Component/Form/Factory/FormFactory.php
@@ -3,13 +3,30 @@
 namespace Nuxia\Component\Form\Factory;
 
 use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\FormTypeInterface;
 
 class FormFactory
 {
+    /**
+     * @var FormFactoryInterface
+     */
     private $formFactory;
+
+    /**
+     * @var string|FormTypeInterface
+     */
     private $type;
+
+    /**
+     * @var array
+     */
     private $options;
 
+    /**
+     * @param FormFactoryInterface     $formFactory
+     * @param string|FormTypeInterface $type
+     * @param array                    $options
+     */
     public function __construct(FormFactoryInterface $formFactory, $type, array $options = array())
     {
         $this->formFactory = $formFactory;
@@ -17,16 +34,28 @@ class FormFactory
         $this->options = $options;
     }
 
+    /**
+     * @param array $options
+     */
     public function setOptions(array $options)
     {
         $this->options = $options;
     }
 
+    /**
+     * @param mixed $data
+     */
     public function setData($data)
     {
         $this->options['data'] = $data;
     }
 
+    /**
+     * @param  mixed|null $data
+     * @param  array      $options
+     *
+     * @return \Symfony\Component\Form\FormInterface
+     */
     public function createForm($data = null, array $options = array())
     {
         if ($data != null) {
