283dc5404968bea1f3de6ae27098d2aee4fcfcda
Yannick Snobbert <yannick.snobbert@gmail.com>
2016-03-04 11:20:28 +0100

    Suppresion du type reset (inutile)

--- src/Nuxia/Component/Form/Type/FilterType.php
+++ src/Nuxia/Component/Form/Type/FilterType.php
@@ -3,6 +3,7 @@
 namespace Nuxia\Component\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
